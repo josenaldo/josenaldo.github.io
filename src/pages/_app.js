814495f4c4efd8182e5629d85094662e07ae2d18
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-04-12 17:54:08 -0300

    :sparkles: feat: Base theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+import theme from '@/styles/theme'
+import AppLayout from '@/components/layout/AppLayout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </ThemeProvider>
+  )
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles'
+import { orange } from '@mui/material/colors'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#00c06e',
+    },
+    secondary: {
+      main: '#ef618a',
+    },
+  },
+})
+
+export default theme
