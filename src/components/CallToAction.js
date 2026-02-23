e773fdc0c2fc8b97ab69e896ca2bf11a1a63b3b7
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2026-02-23 20:30:28 -0300

    feat: atualizar componente CallToAction e substituir no Hero
    - Adicionado o prop 'variant' ao componente CallToAction
    - Atualizado o componente Hero para usar botões diretamente
    - Removido o uso do componente CallToAction em favor de botões personalizados

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,7 +1,7 @@
 import { Box, Button } from '@mui/material'
 import PropTypes from 'prop-types'
 
-const CallToAction = ({ children, href, ariaLabel }) => {
+const CallToAction = ({ children, href, ariaLabel, variant }) => {
     return (
         <Box
             sx={{
@@ -11,17 +11,22 @@ const CallToAction = ({ children, href, ariaLabel }) => {
                 mt: 2,
             }}
         >
-            <Button variant="contained" href={href} aria-label={ariaLabel}>
+            <Button variant={variant} href={href} aria-label={ariaLabel}>
                 {children}
             </Button>
         </Box>
     )
 }
 
+CallToAction.defaultProps = {
+    variant: 'contained',
+}
+
 CallToAction.propTypes = {
     children: PropTypes.node.isRequired,
     href: PropTypes.string.isRequired,
     ariaLabel: PropTypes.string,
+    variant: PropTypes.string,
 }
 
 export default CallToAction
