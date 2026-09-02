7b602011309b1f5460dc90ebec4efb55f848d66f
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2026-09-01 21:20:26 -0300

    feat(design-system): MobileMenu com accordion de About e estado ativo

--- src/layouts/MobileMenu.js
+++ src/layouts/MobileMenu.js
@@ -2,10 +2,13 @@
 
 import React from 'react'
 
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import MenuIcon from '@mui/icons-material/Menu'
 import InboxIcon from '@mui/icons-material/MoveToInbox'
 import {
     Box,
+    Collapse,
     Divider,
     IconButton,
     List,
@@ -18,12 +21,17 @@ import {
 import { useTranslations } from 'next-intl'
 import PropTypes from 'prop-types'
 
-import { Link } from '@/i18n/navigation'
+import { Link, usePathname } from '@/i18n/navigation'
 import LanguageSwitcher from '@/layouts/LanguageSwitcher'
 
-const MobileMenu = ({ pages }) => {
+const isActivePage = (pathname, url) =>
+    url === '/' ? pathname === '/' : pathname.startsWith(url)
+
+const MobileMenu = ({ pages, aboutSubNav }) => {
     const t = useTranslations('Nav')
+    const pathname = usePathname()
     const [drawerOpen, setDrawerOpen] = React.useState(false)
+    const [aboutOpen, setAboutOpen] = React.useState(false)
 
     const handleOpenNavMenu = () => {
         setDrawerOpen(true)
@@ -31,6 +39,10 @@ const MobileMenu = ({ pages }) => {
 
     const handleCloseNavMenu = () => {
         setDrawerOpen(false)
+    }
+
+    const handleToggleAbout = () => {
+        setAboutOpen((open) => !open)
     }
 
     return (
@@ -60,20 +72,93 @@ const MobileMenu = ({ pages }) => {
                 }}
             >
                 <List>
-                    {pages.map((page) => (
-                        <ListItem key={page.url} disablePadding>
-                            <ListItemButton
-                                component={Link}
-                                href={page.url}
-                                onClick={handleCloseNavMenu}
-                            >
-                                <ListItemIcon>
-                                    {page.icon ? <page.icon /> : <InboxIcon />}
-                                </ListItemIcon>
-                                <ListItemText primary={t(page.name)} />
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
+                    {pages.map((page) =>
+                        page.name === 'about' ? (
+                            <React.Fragment key={page.name}>
+                                <ListItem disablePadding>
+                                    <ListItemButton
+                                        component={Link}
+                                        href={page.url}
+                                        selected={isActivePage(
+                                            pathname,
+                                            page.url
+                                        )}
+                                        onClick={handleCloseNavMenu}
+                                    >
+                                        <ListItemIcon>
+                                            {page.icon ? (
+                                                <page.icon />
+                                            ) : (
+                                                <InboxIcon />
+                                            )}
+                                        </ListItemIcon>
+                                        <ListItemText primary={t(page.name)} />
+                                    </ListItemButton>
+                                    <IconButton
+                                        onClick={handleToggleAbout}
+                                        aria-label={t('aboutSubNavLabel')}
+                                        aria-expanded={aboutOpen}
+                                    >
+                                        {aboutOpen ? (
+                                            <ExpandLessIcon />
+                                        ) : (
+                                            <ExpandMoreIcon />
+                                        )}
+                                    </IconButton>
+                                </ListItem>
+                                <Collapse in={aboutOpen} timeout="auto">
+                                    <List disablePadding>
+                                        {aboutSubNav.map((item) => (
+                                            <ListItem
+                                                key={item.name}
+                                                disablePadding
+                                            >
+                                                <ListItemButton
+                                                    component={Link}
+                                                    href={item.url}
+                                                    selected={isActivePage(
+                                                        pathname,
+                                                        item.url
+                                                    )}
+                                                    onClick={handleCloseNavMenu}
+                                                    sx={{ pl: 4 }}
+                                                >
+                                                    <ListItemIcon>
+                                                        {item.icon ? (
+                                                            <item.icon />
+                                                        ) : (
+                                                            <InboxIcon />
+                                                        )}
+                                                    </ListItemIcon>
+                                                    <ListItemText
+                                                        primary={t(item.name)}
+                                                    />
+                                                </ListItemButton>
+                                            </ListItem>
+                                        ))}
+                                    </List>
+                                </Collapse>
+                            </React.Fragment>
+                        ) : (
+                            <ListItem key={page.url} disablePadding>
+                                <ListItemButton
+                                    component={Link}
+                                    href={page.url}
+                                    selected={isActivePage(pathname, page.url)}
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <ListItemIcon>
+                                        {page.icon ? (
+                                            <page.icon />
+                                        ) : (
+                                            <InboxIcon />
+                                        )}
+                                    </ListItemIcon>
+                                    <ListItemText primary={t(page.name)} />
+                                </ListItemButton>
+                            </ListItem>
+                        )
+                    )}
                 </List>
 
                 <Divider />
@@ -88,6 +173,13 @@ const MobileMenu = ({ pages }) => {
 
 MobileMenu.propTypes = {
     pages: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            url: PropTypes.string.isRequired,
+            icon: PropTypes.elementType,
+        })
+    ).isRequired,
+    aboutSubNav: PropTypes.arrayOf(
         PropTypes.shape({
             name: PropTypes.string.isRequired,
             url: PropTypes.string.isRequired,
